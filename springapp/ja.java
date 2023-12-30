331f5c7b138645b3febb95058ab8931dbe9f7dad
neojarvis <techadmin@iamneo.ai>
2023-12-30 11:25:11 +0000

    Auto-save-commit-12/30/2023--4:54:59-PM

--- springapp/ja.java
+++
@@ -1,9 +0,0 @@
-import ch.qos.logback.core.net.SyslogOutputStream;
-
-public class ja {
-    console.log("saravana");
-    console.log("vasantgh")
-    console.log("jana");
-    console.log("metal cutting");
-    console.log()
-}
